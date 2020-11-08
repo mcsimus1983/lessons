dc84e34e2a5b9811c7f1b611ac9f9f1954676809
mcsimus1983 <60735633+mcsimus1983@users.noreply.github.com>
2020-11-08 20:53:00 +0700

    les6 hw

--- src/lesson6/HW/MainClass.java
+++ src/lesson6/HW/MainClass.java
@@ -1,10 +1,20 @@
 package lesson6.HW;
 
 public class MainClass {
-     void main(String[] args) {
+     public static void main(String[] args) {
 
+         Dog dog = new Dog("Мухтар", 400, 0.5, 10);
+         Cat cat = new Cat("Барсик", 100, 2, 0);
+         Dog dog1 = new Dog("Шарик", 200, 0.5, 10);
 
+         Animals[] animals = {dog, cat, dog1};
 
-    }
+         for (Animals animal : animals) {
+             System.out.println(animal);
+             animal.Run(300);
+             animal.Jump(1.5);
+             animal.Sail(5);
+         }
+     }
 
 }
