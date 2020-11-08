e420bccba79cb9b8d697bc5c091eab9149e712bc
mcsimus1983 <60735633+mcsimus1983@users.noreply.github.com>
2020-11-08 16:07:21 +0700

    les7

--- src/lesson6/HW/MainClass.java
+++ src/lesson6/HW/MainClass.java
@@ -3,9 +3,8 @@ package lesson6.HW;
 public class MainClass {
      void main(String[] args) {
 
+
+
     }
 
-    public void main(int args) {
-        
-    }
 }
--- src/lesson7/hw/MainClass.java
+++ src/lesson7/hw/MainClass.java
@@ -2,15 +2,75 @@ package lesson7.hw;
 
 public class MainClass {
 
+
     public static void main(String[] args) {
+
+        Cat[] arrCats = createArrCats();
+        Plate[] plats = createArrPlats();
+
+        fillPlats(plats);
+
+        printInfo(plats);
+        startFeeding(arrCats, plats);
+        printInfo(arrCats);
+        printInfo(plats);
+
+    }
+
+    private static void startFeeding(Cat[] arrCats, Plate[] plats) {
+        for (Cat arrCat : arrCats) {
+            for (Plate plat : plats) {
+
+                if (plat.getFood() != 0 && !arrCat.isSatiety()) {
+                    if (arrCat.eat(plat)) {
+                        break;
+                    }
+
+                }
+
+            }
+        }
+    }
+
+    private static void printInfo(Plate[] plate) {
+        for (Plate value : plate) {
+            value.printInfo();
+        }
+
+    }
+
+    private static void printInfo(Cat[] cats) {
+
+        for (Cat cat : cats) {
+            cat.printInfo();
+        }
+
+    }
+
+    private static void fillPlats(Plate[] plate) {
+        for (Plate value : plate) {
+            value.addFood((int) (Math.random() * 10) + 3);
+        }
+    }
+
+    private static Plate[] createArrPlats() {
+        Plate plate = new Plate(10);
+        Plate plate1 = new Plate(10);
+        Plate plate2 = new Plate(10);
+
+        return new Plate[]{plate, plate1, plate2};
+
+    }
+
+    public static Cat[] createArrCats() {
+
         Cat cat = new Cat("Борис Николаевич", 3);
-        Plate plate = new Plate(5);
+        Cat cat1 = new Cat("Барсик", 4);
+        Cat cat2 = new Cat("Мурзик", 5);
+        Cat cat3 = new Cat("Мурка", 2);
+        Cat cat4 = new Cat("Бандит", 6);
 
-        plate.addFood(4);
-        plate.printInfo();
-        cat.eat(plate);
-        plate.printInfo();
-
+        return new Cat[]{cat, cat1, cat2, cat3, cat4};
 
     }
 }
